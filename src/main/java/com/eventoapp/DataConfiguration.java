1719ada798b5a77a522fa71ee178fe47c851de3b
victorhgb <victorhugo-brito@hotmail.com>
2020-11-11 20:34:51 -0300

    configurando banco

--- src/main/java/com/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -1,9 +1,12 @@
 package com.eventoapp;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,53 +19,56 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 
-    @Bean(name = "dataSourceEvento")
-    @Primary
-    public DataSource EventoDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=master");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("SA_PASSWORD=yourStrong(!)Password");
-		return dataSource;
-    }
-    
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		vendorAdapter.setDatabase(Database.SQL_SERVER);
-		vendorAdapter.setGenerateDdl(true);
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-		em.setDataSource(EventoDataSource());
-		em.setPackagesToScan("com.eventoapp.models");
-		em.setJpaVendorAdapter(vendorAdapter);
-		em.setJpaProperties(additionalProperties());
-		return em;
-	}
-	
-	private Properties additionalProperties() {
-		Properties properties = new Properties();
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-//		properties.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.SQLServerDialect");
-//		properties.setProperty("hibernate.transaction.flush_before_completion", "true");
-		properties.setProperty("hibernate.current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
-		return properties;
-	}
-	
-	@Bean("dataSourceSMA")
-	public DataSource sma() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		dataSource.setUrl("jdbc:sqlserver://s2-sql-conf:1433;databaseName=DBSISTEMAS");
-		dataSource.setUsername("UserPreCred");
-		dataSource.setPassword("[6>tOWK,");
-		return dataSource;
-	}
-	
-	@Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(sma());
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
+	
+	
+//    @Bean(name = "dataSourceEvento")
+//    @Primary
+//    public DataSource EventoDataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//		dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=master");
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("SA_PASSWORD=yourStrong(!)Password");
+//		return dataSource;
+//    }
+    
+//	@Bean
+//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//		vendorAdapter.setDatabase(Database.SQL_SERVER);
+//		vendorAdapter.setGenerateDdl(true);
+//
+//		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//		em.setDataSource(EventoDataSource());
+//		em.setPackagesToScan("com.eventoapp.models");
+//		em.setJpaVendorAdapter(vendorAdapter);
+//		em.setJpaProperties(additionalProperties());
+//		return em;
+//	}
+	
+//	private Properties additionalProperties() {
+//		Properties properties = new Properties();
+//		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+////		properties.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.SQLServerDialect");
+////		properties.setProperty("hibernate.transaction.flush_before_completion", "true");
+//		properties.setProperty("hibernate.current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+//		return properties;
+//	}
+	
 	
 }
