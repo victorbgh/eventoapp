1cdb8a00759b9af49e435e342848cf5f5c4dfed5
victorhgb <victorhugo-brito@hotmail.com>
2020-11-11 21:00:11 -0300

    mudando

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -19,7 +19,7 @@ public class Evento implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long codigo;
 
 	@NotEmpty
